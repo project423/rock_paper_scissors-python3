bde5c74ea132fbc1d66367039f71ff7de83f65a5
Andrew Clark <drewboy879@gmail.com>
2020-07-07 11:38:59 -0500

    added repeat and keeps track of computer and player score

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,7 +4,8 @@ print('Welcome to rock paper scissors')
 # player_two = input("Enter player twos choice: ")
 # player_one = player_one.lower()
 play = True
-while play:
+score = [0,0]
+while play and max(score) < 3:
     player_one = input("Enter player ones choice: ").lower()
     player_two = choice(['rock', 'paper', 'scissors'])
     if player_one == player_two:
@@ -12,21 +13,35 @@ while play:
     elif player_one == 'rock':
         if player_two == 'scissors':
             print('Player one wins')
+            score[0] +=1
         elif player_two == 'paper':
             print('Player two wins')
+            score[1] +=1
     elif player_one == 'scissors':
         if player_two == 'rock':
             print('Player two wins')
+            score[1] +=1
         elif player_two == 'paper':
             print('Player one wins')
+            score[0] +=1
     elif player_one == 'paper':
         if player_two == 'rock':
             print('Player one wins')
+            score[0] +=1
         elif player_two == 'scissors':
             print('Player two wins')
+            score[1] +=1
     print(f'Player one chose {player_one}, and the computer chose {player_two}')
-    play_again = input("Do you want to play again y/n?").lower()
-    if play_again != 'y':
-        play = False
+    print(f'The computer has a score of {score[1]} and you have a score of {score[0]}')
+    if score[0] == 3:
+        print('Congrats you win!')
+    elif score[1] == 3:
+        print('Sorry, the computer won')
+    if max(score) == 3:
+        score = [0,0]
+        play_again = input("Do you want to play again y/n? ").lower()
+        if play_again != 'y':
+            
+            play = False
         
     
